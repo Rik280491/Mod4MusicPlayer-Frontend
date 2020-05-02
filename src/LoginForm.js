e587e6e672989a2b3a0acf565838e70f9943bd08
RikBala <rik280491@googlemail.com>
2020-05-03 00:28:42 +0100

    Styling for login and signup almost done. Style bug in login form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -112,9 +112,9 @@ export default function LoginForm({handleChange, handleSubmit}) {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                {/* <Link href="#" variant="body2">
                   {/* Forgot password? */}
-                </Link>
+                {/* </Link> */} 
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
--- src/Signup.js
+++ src/Signup.js
@@ -1,45 +1,32 @@
-import React from 'react'
-import API from './API'
+import React from "react";
+import API from "./API";
+import SignupForm from "./SignupForm";
 
 export default class Signup extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            username: "",
-            password: ""
-        }
-    }
+	constructor() {
+		super();
+		this.state = {
+			username: "",
+			password: "",
+		};
+	}
 
-    handleChange = (e) => {
-        this.setState({
-          [e.target.name]: e.target.value
-        })
-      }
-      handleSubmit = (e) => {
-        e.preventDefault()
-        API.SignUp(this.state).then(json => console.log(json))
-      }
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+	handleSubmit = (e) => {
+		e.preventDefault();
+		API.SignUp(this.state).then((json) => console.log(json));
+	};
 
-
-
-
-    render() {
-        return(
-          <form onSubmit={this.handleSubmit}>
-            <label>Username:</label>
-            <input type="text" name="username" onChange={this.handleChange}/><br/>
-    
-            <label>Password:</label>
-            <input type="password" name="password" onChange={this.handleChange}/><br/>
-    
-            <input type="submit" value="Sign Up"/>
-          </form>
-        )
-      }
-
-
-
+	render() {
+		return (
+			<SignupForm
+				handleChange={this.handleChange}
+				handleSubmit={this.handleSubmit}
+			/>
+		);
+	}
 }
-
-
-
