4208872bbc99f561c7d1d70a08f5dc90ee12a4cf
RikBala <rik280491@googlemail.com>
2020-05-02 23:42:58 +0100

    log in form styling done

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ const logInURL = `${baseURL}/log-in`
 const validateURL = `${baseURL}/validate`
 const createUserURL = `${baseURL}/users`
 
-// Make a post request to a given URL with a given data object as the body and return the Promise
+
 const post = (url, data) => {
   const configObject = {
     method: "POST",
@@ -16,17 +16,17 @@ const post = (url, data) => {
   return fetch(url, configObject)
 }
 
-// Make a get request to a given URL and return the Promise. If a token has been provided, include it as a header called Authorization
+
 const get = (url, token) => {
   return token ? fetch(url, { headers: { AUTHORIZATION: token } }) : fetch(url)
 }
 
-// Use the get function to make a request to the validate route and parse the response into JSON
+
 const validate = token => {
   return get(validateURL, token).then(response => response.json())
 }
 
-// Use the post function to make a request to the validate route and parse the response into JSON
+
 const LogIn = data => {
   return post(logInURL, data).then(response => response.json())
   
--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import "./App.css";
 import Login from "./Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import API from "./API"
-import Signup from "./Signup"
-
+import API from "./API";
+import Signup from "./Signup";
 
 class App extends React.Component {
 	constructor() {
@@ -14,38 +13,30 @@ class App extends React.Component {
 		};
 	}
 
-
-  componentDidMount() {
-    // If we have a token in localStorage, attempt to use it to validate ourselves against the server
-    if (localStorage.token) {
-      API.validate(localStorage.token)
-      // Pass the username and token the server sends back to signIn
-        .then(json => this.LogIn(json.username, json.token))
-    }
-  }
+	componentDidMount() {
+		if (localStorage.token) {
+			API.validate(localStorage.token)
+			.then((json) => this.LogIn(json.username, json.token));
+		}
+	}
 
 	LogIn = (username, token) => {
-		// Set the state of username to be the username the server sent back
 		this.setState({
 			username,
 		});
-		// Store the token the server sent back in localStorage, which is on the client-side
+
 		localStorage.token = token;
 	};
 
 	render() {
 		return (
 			<div className="App">
-				<h1>HI</h1>
-				<Router>
-        <Route
-						exact
-						path="/sign-up"
-						component={() => <Signup />}
-					/>
+				
+
+      	<Router>
+					<Route exact path="/sign-up" component={() => <Signup />} />
           
-          
-          <Route
+					<Route
 						exact
 						path="/log-in"
 						component={() => <Login LogIn={this.LogIn} />}
